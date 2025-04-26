98b8d871fcfb0e76441b46227246fa6a211796f0
Scott Osteen <osteensco@gmail.com>
2025-04-25 22:32:34 -0500

    WIP py SDK API

--- py/src/switchboard/workflow.py
+++ py/src/switchboard/workflow.py
@@ -1,6 +1,6 @@
 import json
 from dataclasses import dataclass
-from typing import Callable
+from typing import Self
 
 
 
@@ -17,7 +17,9 @@ class Registry:
 class Step:
     run_id: list[int]
     name: str
-    fn: Callable
+    execution_type: str # make enum
+    executed: bool = False
+    completed: bool = False
     success: bool = False
 
 
@@ -30,8 +32,13 @@ class State:
 @dataclass
 class Context:
     run_id: list[int]
+    executed: bool
+    completed: bool
     success: bool
 
+class WaitStatus:
+    def call(self) -> Self:
+        return self
 
 # context = {
 #             "run_id": [
@@ -55,6 +62,7 @@ class Workflow:
         self.state = self._init_state(db)
 
 
+
         # workflow is stepped through by tracking the index (number of steps completed) and current count (current index in steps)
         # TODO
         #   - setup caching for conditionals, loops, etc
@@ -73,12 +81,10 @@ class Workflow:
     def _get_context(self, context) -> Context:
         raw = json.loads(context) 
         try:
-            cntx = Context(raw["run_id"], raw["success"])
-        except: # newly triggered workflows wont have a run_id or success field
-            cntx = Context(self._generate_id(), False)
-
-        if cntx.run_id[0] == 0:
-            return cntx
+            cntx = Context(raw["run_id"], raw["executed"], raw["completed"], raw["success"])
+        except: 
+            # newly triggered workflows wont have these fields
+            cntx = Context(self._generate_id(), True, True, True)
 
         return cntx
         
@@ -89,16 +95,22 @@ class Workflow:
         if not state:
             # handle new state creation (new workflow run)
             state = State([], self.context.run_id[0])
+            return state
+
+        # TODO 
+        # assert steps is populated
+        if self.context.executed:
+            state.steps[-1].executed = True
+        if self.context.completed:
+            state.steps[-1].completed = True
         if self.context.success:
             state.steps[-1].success = True
-        else:
-            pass
-            # apply retry logic
-            # retry logic should have a default and be defined by the user
+        
         self.step_idx = len(state.steps)-1
+
         return state
 
-    def _update_state(self, step):
+    def _add_step(self, step):
         self.step_idx += 1
         self.state.steps.append(step)
     
@@ -112,34 +124,53 @@ class Workflow:
         #   implement
         return [0,0]
 
-    def _generate_worker_id(self):
+    def _generate_worker_id(self) -> int:
         return self.context.run_id[1]+1
 
-    def _determine_step(self):
-        if self.step_cnt != self.step_idx:
+    def _needs_retry(self) -> bool:
+        if self.context.executed and self.context.completed and not self.context.success:
             return True
         return False
 
-    def _enqueue_function(self, fn):
+    def _is_waiting(self) -> bool:
+        if self.context.executed and self.context.completed and self.context.success:
+            return False
+        return True 
+
+    def _determine_step_execution(self,fn) -> bool:
+        if not self._is_waiting():
+            self._add_step(Step([self.state.run_id, self._generate_worker_id()], "call", fn))
+            return True
+        if self._needs_retry():
+            return True
+        return False
+
+    def _enqueue_execution(self, fn):
         print(f"Enqueuing function call: {fn['id']}")
 
         # Example message schema
         # queue.send_message({
         #     "function_id": fn["id"],
-        #     "workflow_state": self.state
+        #     "execution_type": "http"
         # })
 
-    def _next(self):
+    def _next(self) -> Self:
         self.step_cnt += 1
         return self
 
-    def call(self, fn):
+
+    def call(self, fn) -> Self | WaitStatus:
+
+        if self._determine_step_execution(fn):
+            # walk through orchestration steps until we are at current call
+            if self.step_cnt != self.step_idx:
+                return self._next()
+            
+            self._enqueue_execution(fn)
+        
         self._update_db(self.db)
-        if not self._determine_step():
-            return self._next()
-        self._update_state(Step([self.state.run_id, self._generate_worker_id()], "call", fn))
-        self._enqueue_function(fn)
-        return 
+        return WaitStatus()
+
 
     def done(self):
         return
--- py/tests/workflow_test.py
+++ py/tests/workflow_test.py
@@ -1,19 +1,75 @@
-import pytest
-from switchboard.workflow import Workflow, Context
+from switchboard.workflow import State, Workflow, Context, Step
+
+
 
 class DBMock: 
+    def __init__(self, state: State | None) -> None:
+        self.state = state if state else None
+
     def read(self, *args):
-        return None
+        return self.state
 
     def write(self, *args):
-        return None
+        return self.state
 
-db = DBMock()
 
-def test_something():
+def test_new_Workflow():
     context = '{}'
-    workflow = Workflow(db, context)
-    expected = Context([0,0], False)
-    assert workflow._get_context(context) == expected
+    db = DBMock(None)
+    wf = Workflow(db, context)
+    expected_context = Context([0,0], True, True, True)
+    expected_state = State([],0)
+    assert wf.context == expected_context
+    assert wf.state == expected_state
+
+def test_get_context():
+    wf = Workflow.__new__(Workflow)
+    context = '{"run_id": [100,1], "executed": true, "completed": true, "success": true}'
+    expected = Context([100,1], True, True, True)
+    actual = wf._get_context(context) 
+    assert actual == expected
+
+def test_init_state(): 
+    context = Context([100,2], True, True, True)
+    expected = State([Step([100,1],"call","http",True,True,True), Step([100,2],"call","http",True,True,True)],100)
+    db = DBMock(expected)
+    wf = Workflow.__new__(Workflow)
+    wf.context = context
+    actual = wf._init_state(db)
+    assert actual == expected
+
+def test_add_step():
+    wf = Workflow.__new__(Workflow)
+    wf.step_idx = 0
+    wf.state = State([],100)
+    wf._add_step(Step([100,1],"call","http",True,True,True))
+    wf._add_step(Step([100,2],"call","http",True,True,True))
+    expected = State([Step([100,1],"call","http",True,True,True), Step([100,2],"call","http",True,True,True)],100) 
+    actual = wf.state
+    assert actual == expected
+
+def test_generate_id():
+    pass
+
+def test_needs_retry():
+    wf = Workflow.__new__(Workflow)
+    context = Context([100,1], True, True, False)
+    wf.context = context
+
+
+def test_is_waiting():
+    pass
+
+def test_enqueue_function():
+    pass
+
+def test_next():
+    pass
+
+def test_call():
+    pass
+
+
+
 
 
