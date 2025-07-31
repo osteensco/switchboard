2e0ae8dd5bd7e4d03a0a41543aed466c4f52f2b7
Scott Osteen <osteensco@gmail.com>
2025-07-30 21:55:19 -0500

    TODO

--- cli/src/core/scaffolder.go
+++ cli/src/core/scaffolder.go
@@ -4,22 +4,25 @@ import (
 	"fmt"
 )
 
-// TODO
-// 	- init
-// 		- brings up wizard (bubbletea, non tui) to select options
-// 		- all options should correspond to flags and arguments
-
 func InitProject(cloud string, lang string) {
+	// TODO
+	// - handle project name
+	// - generate project structure
+	// - populate project with template files
+	// - generate config file
+
 	switch cloud {
 	case "aws", "gcp", "azure":
 		fmt.Printf("Init New Project in %s\n", cloud)
 	default:
 		fmt.Printf("'%s' is an invalid option, please use one of 'aws', 'gcp', or 'azure'\n", cloud)
+		return
 	}
 	switch lang {
 	case "py", "ts", "go":
 		fmt.Printf("Init New %s Project\n", lang)
 	default:
 		fmt.Printf("'%s' is an invalid option, please use one of 'py', 'ts', or 'go'\n", lang)
+		return
 	}
 }
