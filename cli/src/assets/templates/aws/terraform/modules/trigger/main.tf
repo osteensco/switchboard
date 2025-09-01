a153f7c6a1436c9dffb5e798aa2bc7afd7aa0b90
Scott Osteen <osteensco@gmail.com>
2025-09-01 13:58:38 -0500

    Updated trigger terraform skeleton

---
+++ cli/src/assets/templates/aws/terraform/modules/trigger/main.tf
@@ -0,0 +1,3 @@
+# This module acts as a container for Switchboard trigger resources.
+# Trigger-specific resources are added here by the cli tool and will be in their own specific terraform files.
+# You can have multiple triggers for any given Switchboard workflow.
